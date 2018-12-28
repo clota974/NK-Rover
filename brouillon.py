a73d6cfd8748072e16ade914fbee541aa8acaecc
Killian <clota974@gmail.com>
2018-12-29 01:59:55 +0400

    Update brouillon.py

--- brouillon.py
+++ brouillon.py
@@ -1,13 +1,8 @@
-# pylint: disable=import-error
 from smbus2 import SMBusWrapper
-import os
-
-clear = lambda: os.system('clear')
 
 while True:
     with SMBusWrapper(1) as bus:
         # Read a block of 16 bytes from address 80, offset 0
-        clear()
         block = bus.read_i2c_block_data(0x60, 0, 16)
         # Returned value is a list of 16 bytes
         print(block)
