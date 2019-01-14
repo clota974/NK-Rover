afab7113c797743e5baaaa4acabe040acea1c637
Killian <clota974@gmail.com>
2019-01-14 16:09:31 +0400

    Create rawhid.py

---
+++ rawhid.py
@@ -0,0 +1,26 @@
+import os, sys, datetime
+from time import sleep
+from io import FileIO
+
+report_fd = os.open("/dev/input/js1", os.O_RDWR | os.O_NONBLOCK)
+fd = FileIO(report_fd, "rb+", closefd=False)
+
+defBuf = bytearray(214)
+
+while True:
+    buf = defBuf 
+    r = fd.readinto(buf)
+    arr = []
+    sign = buf[2]
+    print("Sign = ", sign)
+
+    for i in buf:
+        if(buf[i]==sign and buff[i+1]==0):
+            arr.append(buff[i+2]) 
+            arr.append(buff[i+3]) 
+
+    p = ' '.join(format(x, '02x') for x in arr)
+    p = p.replace("\r", "")
+    print("\r"+p, end="") 
+    sleep(0.1)
+    sys.stdout.flush()
