f1dadae9e0d1fa974c7a1cc1ed0a1544171b1bbc
Killian <clota974@gmail.com>
2019-01-14 18:15:08 +0400

    Update brouillon2.py

--- brouillon2.py
+++ brouillon2.py
@@ -4,6 +4,6 @@ import sys
 for i in range(10):
     buf = [1,2,3,4,5,i]
     p = ' '.join(format(x, '02x') for x in buf)
-    print("\r"+p+"\r"+p, end="") 
+    print("\r"+p+"\n"+p, end="") 
     time.sleep(0.5)
     sys.stdout.flush()
