8dfdcca46430052e8aeec53ac0e5deb30f182b29
Killian <clota974@gmail.com>
2019-01-14 14:56:52 +0400

    Update hidtest.py

--- hidtest.py
+++ hidtest.py
@@ -5,12 +5,13 @@ from io import FileIO
 report_fd = os.open("/dev/input/js1", os.O_RDWR | os.O_NONBLOCK)
 fd = FileIO(report_fd, "rb+", closefd=False)
 
-defBuf = bytearray(214)
+defBuf = bytearray(100)
 
 while True:
     buf = defBuf 
     r = fd.readinto(buf)
     p = ' '.join(format(x, '02x') for x in buf)
+    print(buf)
     print("\r"+p, end="") 
     sleep(0.2)
     sys.stdout.flush()
