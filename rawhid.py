b5585015e92ccb822dc57ce284e3547cb225cf5a
Killian <clota974@gmail.com>
2019-01-14 16:40:35 +0400

    Update rawhid.py

--- rawhid.py
+++ rawhid.py
@@ -5,7 +5,7 @@ from io import FileIO
 report_fd = os.open("/dev/input/js1", os.O_RDWR | os.O_NONBLOCK)
 fd = FileIO(report_fd, "rb+", closefd=False)
 
-defBuf = bytearray(230)
+defBuf = bytearray(240)
 
 while True:
     buf = defBuf 
